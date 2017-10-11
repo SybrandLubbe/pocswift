0eac112b46c8464befee726c237a9fe28048d3fa
Sybrand Lubbe <sybrandlubbe@Sybrands-MacBook-Air.local>
2017-10-11 09:27:45 +0200

    small change

--- POCSwiftAnalytics/Deep1ViewController.swift
+++ POCSwiftAnalytics/Deep1ViewController.swift
@@ -14,6 +14,7 @@ class Deep1ViewController: UIViewController {
         super.viewDidLoad()
         
         self.title = "Deep Link"
+        //This is where you add the analytics code:
         
         /*
         ADBMobile.trackState("PageName", data: [
